d9e2d637f3a3149ef5b9634d85c865050f968212
OmerEnsarshn <250542022@firat.edu.tr>
2025-10-27 16:05:10 +0300

    Update MaasHesap.java

--- MaasHesap.java
+++ MaasHesap.java
@@ -1,1 +1,9 @@
-
+/*
+ * Ad Soyad: Ömer Ensar Şahin
+ * Ogrenci No: 250542022
+ * Tarih: 27.10.2025
+ * Aciklama: Gorev 3 - Maaş Hesap
+ * 
+ * 
+ * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
+ */
